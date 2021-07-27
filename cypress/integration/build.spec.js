712f1193fa355f598b1f162ce45d2c6b22e9d13f
ChristianMLux <christian.m.lux@gmail.com>
2021-07-27 17:11:31 +0200

    feat: implements addTodo test

--- cypress/integration/build.spec.js
+++ cypress/integration/build.spec.js
@@ -8,4 +8,13 @@ describe("todo app", () => {
   it("radio all should be checked", () => {
     cy.get("[data-cy=radioAll").should("be.checked");
   });
+  // add new todo
+  it("should add a new todo to the list", () => {
+    cy.get("[data-cy=addTodoTf").type("Test Todo");
+    cy.get("[data-cy=addTodoBtn").click();
+    cy.get("[data-cy=todoList]")
+      .should("have.length", 1)
+      .last()
+      .should("have.text", "Test Todo");
+  });
 });
